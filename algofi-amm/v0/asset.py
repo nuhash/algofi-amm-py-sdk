0816971d87e18e219ac992a4a609ac4be0ab1cdb
John Clarke <john@algofi.org>
2022-01-25 22:16:06 +0000

    added logic sig gen + asset data

--- algofi-amm/v0/asset.py
+++ algofi-amm/v0/asset.py
@@ -5,6 +5,12 @@ import pprint
 class Asset():
 
     def __init__(self, amm_client, asset_id):
+        """Constructor method for :class:`Asset`
+        :param amm_client: a :class:`AlgofiAMMClient` for interacting with the AMM
+        :type amm_client: :class:`AlgofiAMMClient`
+        :param asset_id: asset id
+        :type asset_id: int
+        """
         asset_info = amm_client.algod.asset_info(asset_id)
         self.asset_id = asset_id
         self.creator = asset_info['params']['creator']
@@ -19,6 +25,10 @@ class Asset():
         self.url = asset_info['params']['url']
     
     def __str__(self):
+        """Returns a pretty string representation of the :class:`Asset` object
+        :return: string representation of asset
+        :rtype: str
+        """
         return pprint.pformat({"asset_id": self.asset_id, "creator": self.creator, "decimals": self.decimals, "default_frozen": self.default_frozen,
                 "freeze": self.freeze, "manager": self.manager, "name": self.name, "reserve": self.reserve, "total": self.total,
                 "unit_name": self.unit_name, "url": self.url})
