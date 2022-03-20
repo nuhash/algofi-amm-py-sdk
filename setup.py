bdc96f0b8e1367cade7224d4d44fac236e99a49b
John Clarke <john@algofi.org>
2022-03-20 18:08:27 +0000

    updated version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     description="Algofi AMM Python SDK",
     author="Algofi",
     author_email="founders@algofi.org",
-    version="1.0.2",
+    version="1.0.3",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
